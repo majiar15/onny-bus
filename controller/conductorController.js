9394c36d1bdbe0265314cbb7ca66ab56c2c9e578
martin jimenez <majiar16@gmail.com>
2020-11-22 19:21:00 -0500

    pagination conductor

--- controller/conductorController.js
+++ controller/conductorController.js
@@ -11,42 +11,61 @@ exports.register = function(req, res) {
                 password = req.body.password;
             conductorModel.create({ cc, nombres, apellidos, email, password }, (err, newUser) => {
                 if (err) {
-                    res.render('./conductor/conductores', {error: 'ya hay alguien con la misma cedula'});
+                    res.render('./conductor/registroConductores', {error: 'ya hay alguien con la misma cedula', type: "registro"});
                 } else {
-                    res.redirect("/conductor");
+                    res.render('./conductor/registroConductores', {message: 'conductor creado correctamente', type: "registro"});
                 };
             });
         } else {
-            res.render('./conductor/conductores', {error: 'las contraseñas no coinciden'});
+            res.render('./conductor/registroConductores', {error: 'las contraseñas no coincidenr', type: "registro"});
         }
 
     } else {
-        res.render('./conductor/conductores', {error: 'no se enviaron los parametros necesarios, los cuales son cc,nombre,apellidos,email.password,confirmpassword'});
+        res.render('./conductor/registroConductores', {error: 'no se enviaron los parametros necesarios,los cuales son cc,nombre,apellidos,email.password,confirmpassword', type: "registro"});
     }
 
 }
 
 exports.modificar = function(req, res) {
-    const { cc, nombre, apellidos, email, password } = req.body;
-    conductorModel.updateOne({ 'cc': cc }, (err, conductor) => {
+    const { cc, nombres, apellidos, email, password } = req.body;
+    conductorModel.findOneAndUpdate({ 'cc': cc },{nombres,apellidos,email,password}, (err, conductor) => {
         if (!conductor) {
-            res.render('./conductor/registroConductores', { message: "error al rende" });
+            res.render('./conductor/registroConductores', { message: "error al rende", type:"update"});
         } else if (err) {
-            res.render('./conductor/registroConductores', { message: "datos moficados correctamente" });
+            res.render('./conductor/registroConductores', { err , type: "update" });
         } else {
-            res.render('./conductor/registroConductores', { message: "datos moficados correctamente" });
+            res.render('./conductor/registroConductores', { message: "datos moficados correctamente", type : "update" });
         }
     });
 }
 
 exports.home = function (req,res) {
-    conductorModel.find({},"_id cc nombres apellidos email",(err, conductores) =>{
+    let {num_page} = req.params;
+    num_page = parseInt(num_page)
+    skip_page = (num_page-1)*10;
+    conductorModel.countDocuments().then(function ( count ){
+        num_pages = parseInt((count/10)+1);
+    });
+
+    conductorModel.find({},"_id cc nombres apellidos email")
+    .skip(skip_page)
+    .limit(10)
+    .lean()
+    .exec((err, conductores) =>{
+        
         if(!conductor){
             res.render('./conductor/conductores');
         }else if(err){
+            
             res.render('./conductor/conductores', { message: err });
         }else{
-            res.render('./conductor/conductores', { conductores });
+            context = {
+                conductores : conductores,
+                num_page: num_page,
+                num_pages: num_pages
+            }
+            console.log(context);
+            res.render('./conductor/conductores', context);
         }
     });
 }
--- router/conductor.js
+++ router/conductor.js
@@ -3,8 +3,8 @@ const router = express.Router();
 const conductorController = require('../controller/conductorController');
 
 
-router.get('/',conductorController.home);
-
+router.get('/page/:num_page',conductorController.home);
+router.get('/registro', (req,res) =>{res.render('./conductor/registroConductores')});
 
 router.post('/registro', conductorController.register);
 
