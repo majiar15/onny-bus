6cb91e983156d963cd6550334b2a580d07cccea4
martin jimenez <majiar16@gmail.com>
2020-11-24 19:08:29 -0500

    resolve problems router rutas

--- router/ruta.js
+++ router/ruta.js
@@ -5,14 +5,14 @@ router.get('/asignar', (req, res) => res.render('./rutas/asignarRuta'));
 
 
 router.get('/registro', (req, res) => res.render('./rutas/registroRutas', {type:'registro'}));
-router.post('/registro', busController.register );
+// router.post('/registro', busController.register );
 
-router.get('/page/:num_page',busController.home);
+// router.get('/page/:num_page',busController.home);
 
 
-router.get('/update/:id', busController.updateGet)
-router.post('/update', busController.update)
+// router.get('/update/:id', busController.updateGet)
+// router.post('/update', busController.update)
 
-router.get('/remove/:id', busController.remove);
+// router.get('/remove/:id', busController.remove);
 
 module.exports = router;
